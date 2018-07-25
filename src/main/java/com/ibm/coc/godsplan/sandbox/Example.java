cf44988bdf0e30bd228de2d391510805e64efe6a
Jacob de Vos <jacobwdv@ca.ibm.com>
2018-07-25 14:32:50 -0400

    adding twilio api examples

---
+++ src/main/java/com/ibm/coc/godsplan/sandbox/Example.java
@@ -0,0 +1,24 @@
+package com.ibm.coc.godsplan.sandbox;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class Example
+{
+   // Find your Account Sid and Token at twilio.com/user/account
+   public static final String ACCOUNT_SID = "AC5e443ddc79e385a091623984e1903757";
+   public static final String AUTH_TOKEN = "d99f21bfd7f3b13960356520153b806d";
+   public static final String CLIENT_PHONE_NUMBER = "+14162093379";
+   public static final String SERVER_PHONE_NUMBER = "+16476973928";
+
+   public static void main(String[] args)
+   {
+      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+      Message message = Message.creator(new PhoneNumber(CLIENT_PHONE_NUMBER), new PhoneNumber(SERVER_PHONE_NUMBER),
+            "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+
+      System.out.println(message.getSid());
+   }
+}
