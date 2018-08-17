7e1f4131471e658d1b34145552eb525d8215ba0e
Jacob de Vos <jacobwdv@ca.ibm.com>
2018-08-17 18:33:49 -0400

    refactoring

--- src/main/java/com/ibm/coc/godsplan/assistant/WatsonAssistantBot.java
+++ src/main/java/com/ibm/coc/godsplan/assistant/WatsonAssistantBot.java
@@ -7,24 +7,39 @@ import com.ibm.watson.developer_cloud.assistant.v1.model.InputData;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
 
+/**
+ * A class to interact with the GodsPlan Watson Assistant
+ */
 public class WatsonAssistantBot
 {
 
+   private static final String ENC_PASSWD = "5oZIyaeGU1Pc";
+   private static final String USERNAME = "333a833c-fe0a-4f10-af9e-739f368ff725";
+   private static final String REST_API_VERSION = "2018-02-16";
    private final Assistant servissimo;
    private Optional<MessageResponse> lastResponse;
 
+   /**
+    * zero argument constructor
+    */
    public WatsonAssistantBot()
    {
-      this.servissimo = new Assistant("2018-02-16");
-      this.servissimo.setUsernameAndPassword("333a833c-fe0a-4f10-af9e-739f368ff725", "5oZIyaeGU1Pc");
+      this.servissimo = new Assistant(REST_API_VERSION);
+      this.servissimo.setUsernameAndPassword(USERNAME, ENC_PASSWD);
       this.lastResponse = Optional.empty();
    }
 
+   /**
+    * @param context
+    *           context to provide with the input
+    * @param input
+    *           the message to send to Watson assistant
+    * @return the return text from Watson assistant
+    */
    public String sendAssistantMessage(Optional<Context> context, Optional<InputData> input)
    {
 
       MessageOptions options;
-      String returnOutput = "";
       if (context.isPresent() && input.isPresent())
       {
          options = new MessageOptions.Builder("e9fc9a95-fbfb-4210-b8e8-bd40cb3bebe2").input(input.get())
