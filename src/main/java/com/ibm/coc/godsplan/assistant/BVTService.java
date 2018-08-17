a0c42f210278b6c1ad722208c2acaaab3942acd5
Jacob de Vos <jacobwdv@ca.ibm.com>
2018-08-17 15:35:45 -0400

    removed input from init call

--- src/main/java/com/ibm/coc/godsplan/assistant/BVTService.java
+++ src/main/java/com/ibm/coc/godsplan/assistant/BVTService.java
@@ -1,72 +1,55 @@
 package com.ibm.coc.godsplan.assistant;
 
 import com.ibm.watson.developer_cloud.assistant.v1.Assistant;
-import com.ibm.watson.developer_cloud.assistant.v1.model.InputData;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
-import com.ibm.watson.developer_cloud.assistant.v1.model.OutputData;
 import com.ibm.watson.developer_cloud.http.ServiceCallback;
 
-import jersey.repackaged.jsr166e.CompletableFuture;
+public class BVTService
+{
 
-public class BVTService {
+   public static void main(String[] args) throws Exception
+   {
+      Assistant service = new Assistant("2018-02-16");
+      service.setUsernameAndPassword("333a833c-fe0a-4f10-af9e-739f368ff725", "5oZIyaeGU1Pc");
+      MessageOptions options = new MessageOptions.Builder("e9fc9a95-fbfb-4210-b8e8-bd40cb3bebe2").build();
 
-	public static void main(String[] args) throws Exception {
-		Assistant service = new Assistant("2018-02-16");
-		service.setUsernameAndPassword("333a833c-fe0a-4f10-af9e-739f368ff725", "5oZIyaeGU1Pc");
+      // sync
+      MessageResponse response = service.message(options).execute();
+      System.out.println(response);
 
-		InputData input = new InputData.Builder("Yes").build();
-		MessageOptions options = new MessageOptions.Builder("e9fc9a95-fbfb-4210-b8e8-bd40cb3bebe2").input(input)
-				.build();
+      // async
+      service.message(options).enqueue(new ServiceCallback<MessageResponse>()
+      {
+         @Override
+         public void onResponse(MessageResponse response)
+         {
+            System.out.println(response);
+         }
 
-		// sync
-		MessageResponse response = service.message(options).execute();
-		System.out.println(response);
+         @Override
+         public void onFailure(Exception e)
+         {
+         }
+      });
 
-		// async
-		service.message(options).enqueue(new ServiceCallback<MessageResponse>() {
-			@Override
-			public void onResponse(MessageResponse response) {
-				System.out.println(response);
-			}
+      // rx callback
+      service.message(options).rx().thenApply(message -> message.getOutput())
+            .thenAccept(output -> System.out.println(output));
 
-			@Override
-			public void onFailure(Exception e) {
-			}
-		});
+      // rx async callback
+      service.message(options).rx().thenApplyAsync(message -> message.getOutput())
+            .thenAccept(output -> System.out.println(output));
 
-		// rx callback
-		service.message(options).rx().thenApply(new CompletableFuture.Fun<MessageResponse, OutputData>() {
-			@Override
-			public OutputData apply(MessageResponse message) {
-				return message.getOutput();
-			}
-		}).thenAccept(new CompletableFuture.Action<OutputData>() {
-			@Override
-			public void accept(OutputData output) {
-				System.out.println(output);
-			}
-		});
-
-		// rx async callback
-		service.message(options).rx().thenApplyAsync(new CompletableFuture.Fun<MessageResponse, OutputData>() {
-			@Override
-			public OutputData apply(MessageResponse message) {
-				return message.getOutput();
-			}
-		}).thenAccept(new CompletableFuture.Action<OutputData>() {
-			@Override
-			public void accept(OutputData output) {
-				System.out.println(output);
-			}
-		});
-
-		// rx sync
-		try {
-			MessageResponse rxMessageResponse = service.message(options).rx().get();
-			System.out.println(rxMessageResponse);
-		} catch (Exception ex) {
-			// Handle exception
-		}
-	}
+      // rx sync
+      try
+      {
+         MessageResponse rxMessageResponse = service.message(options).rx().get();
+         System.out.println(rxMessageResponse);
+      }
+      catch (Exception ex)
+      {
+         // Handle exception
+      }
+   }
 }
