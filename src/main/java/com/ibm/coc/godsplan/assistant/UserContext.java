e490626c404df8afbf4d7d80204b1d4960c9ed46
petra <petra@petras-mbp.war.can.ibm.com>
2018-08-19 19:00:01 -0400

    add cloudant persistence

---
+++ src/main/java/com/ibm/coc/godsplan/assistant/UserContext.java
@@ -0,0 +1,23 @@
+package com.ibm.coc.godsplan.assistant;
+
+/**
+ * POJO for usercontext database documents
+ *
+ */
+public class UserContext {
+	private String address ;
+	private String _id;
+	
+	public UserContext(String phoneNumber, String address) {
+		this.address = address;
+		this._id = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhoneNumber() {
+		return _id;
+	}	
+}
