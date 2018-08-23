7158492def2dc0c5ee85637af8059e26006c706b
psotornik <Petra.Sotornik@ca.ibm.com>
2018-08-23 16:06:07 -0400

    persist address confirmation wip

--- src/main/java/com/ibm/cfc/godsplan/cloudant/model/LocationContext.java
+++ src/main/java/com/ibm/cfc/godsplan/cloudant/model/LocationContext.java
@@ -8,26 +8,45 @@ import com.ibm.cfc.godsplan.maps.model.GoogleAddressInformation;
  */
 public class LocationContext
 {
-
-   private GoogleAddressInformation location;
    private String _id;
+   private GoogleAddressInformation location;
+   private boolean addressConfirmed;
 
    /**
     * 
     * @param phoneNumber
     * @param location
+    * @param addressConfirmed
     */
-   public LocationContext(String phoneNumber, GoogleAddressInformation location)
+   public LocationContext(String phoneNumber, GoogleAddressInformation location, boolean addressConfirmed)
    {
-      this.location = location;
       this._id = phoneNumber;
+      this.location = location;
+      this.addressConfirmed = addressConfirmed;
    }
 
+   /**
+    * 
+    * @return true if user has confirmed address is correct
+    */
+   public boolean getAddressConfirmed()
+   {
+      return addressConfirmed;
+   }
+   
+   /**
+    * 
+    * @return {@link GoogleAddressInformation}
+    */
    public GoogleAddressInformation getAddress()
    {
       return location;
    }
 
+   /**
+    * 
+    * @return the user phone number
+    */
    public String getPhoneNumber()
    {
       return _id;
