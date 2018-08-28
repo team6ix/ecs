be4aada1b8e6dea054a3f2b5cb30258f8d926d1c
JAISONLoodu <JAISONLoodu@DESKTOP-P4V285I.mshome.net>
2018-08-27 20:32:30 -0400

    getter and setter for hasSpace

--- src/main/java/com/ibm/cfc/godsplan/cloudant/model/SurveyContext.java
+++ src/main/java/com/ibm/cfc/godsplan/cloudant/model/SurveyContext.java
@@ -1,73 +1,84 @@
 package com.ibm.cfc.godsplan.cloudant.model;
 
-import java.util.Optional;
-
 /**
  * 
  */
 public class SurveyContext
 {
-   private String _id;
-   private Boolean mustEvacuate;
-   private Boolean hasVehicle;
-   
-   /**
-    * @return the _id
-    */
-   public String get_id()
-   {
-      return _id;
-   }
-   /**
-    * @param _id the _id to set
-    */
-   public void set_id(String _id)
-   {
-      this._id = _id;
-   }
-   
-   /**
-    * @return the mustEvacuate
-    */
-   public Boolean getMustEvacuate()
-   {
-      return mustEvacuate;
-   }
-   
-   /**
-    * 
-    * @param isMustEvacuate
-    */
-   public void setMustEvacuate(boolean isMustEvacuate)
-   {
-      this.mustEvacuate = isMustEvacuate;
-   }
-   
-   /**
-    * @return the hasVehicle
-    */
-   public Boolean getHasVehicle()
-   {
-      return hasVehicle;
-   }
-   
-   /**
-    * @param hasVehicle the hasVehicle to set
-    */
-   public void setHasVehicle(Boolean hasVehicle)
-   {
-      this.hasVehicle = hasVehicle;
-   }
+	private String _id;
+	private Boolean mustEvacuate;
+	private Boolean hasVehicle;
+	private Boolean hasSpace;
 
-   /**
-    * 
-    */
-   @Override
-   public String toString()
-   {
-      return "SurveyContext [get_id()=" + get_id() + ", getMustEvacuate()=" + getMustEvacuate() + ", getHasVehicle()="
-            + getHasVehicle() + "]";
-   }
+	/**
+	 * @return the _id
+	 */
+	public String get_id()
+	{
+		return _id;
+	}
 
-   
+	/**
+	 * @param _id
+	 *            the _id to set
+	 */
+	public void set_id(String _id)
+	{
+		this._id = _id;
+	}
+
+	/**
+	 * @return the mustEvacuate
+	 */
+	public Boolean getMustEvacuate()
+	{
+		return mustEvacuate;
+	}
+
+	/**
+	 * 
+	 * @param isMustEvacuate
+	 */
+	public void setMustEvacuate(boolean isMustEvacuate)
+	{
+		this.mustEvacuate = isMustEvacuate;
+	}
+
+	/**
+	 * @return the hasVehicle
+	 */
+	public Boolean getHasVehicle()
+	{
+		return hasVehicle;
+	}
+
+	/**
+	 * @param hasVehicle
+	 *            the hasVehicle to set
+	 */
+	public void setHasVehicle(Boolean hasVehicle)
+	{
+		this.hasVehicle = hasVehicle;
+	}
+
+	public Boolean getHasSpace()
+	{
+		return this.hasSpace;
+	}
+
+	public void setHasSpace(Boolean hasSpace)
+	{
+		this.hasSpace = hasSpace;
+	}
+
+	/**
+	* 
+	*/
+	@Override
+	public String toString()
+	{
+		return "SurveyContext [get_id()=" + get_id() + ", getMustEvacuate()=" + getMustEvacuate() + ", getHasVehicle()="
+				+ getHasVehicle() + ", getHasSpace()=" + getHasSpace() + "]";
+	}
+
 }
