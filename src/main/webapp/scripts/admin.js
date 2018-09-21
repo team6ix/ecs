e799c8fd5b378abc815f62ea4489a6eba944448d
Dan To <danto1@ca.ibm.com>
2018-09-21 17:20:15 -0400

    Added js

---
+++ src/main/webapp/scripts/admin.js
@@ -0,0 +1,13 @@
+
+function submitShelter()
+{
+  console.log("Sending request to add new shelter");
+  document.shelterForm.submit();
+}
+
+function submitDisaster()
+{
+  console.log("Sending request to add new disaster");
+  document.disasterForm.submit();
+}
+
