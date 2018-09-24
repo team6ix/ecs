80c134dc6221991e3d6d9a0d9742d070353031b1
Dan To <danto1@ca.ibm.com>
2018-09-24 10:56:06 -0400

    Changed form submission to redirect to same page

--- src/main/webapp/scripts/admin.js
+++ src/main/webapp/scripts/admin.js
@@ -1,14 +1,16 @@
 function submitShelter()
 {
-	alert("Sending request to add new shelter")
 	console.log("Sending request to add new shelter");
 	document.getElementById("shelterForm").submit();
+	window.location.href = "admin.html";
+	alert("Sending request to add new shelter");
 }
 
 function submitDisaster()
 {
-	alert("Sending request to add new disaster");
 	console.log("Sending request to add new disaster");
 	document.getElementById("disasterForm").submit();
+	window.location.href = "admin.html";
+	alert("Sending request to add new disaster");
 }
 
