94dac6743e6ae21a33434e8626bc8080b60c6703
Dan To <danto1@ca.ibm.com>
2018-09-24 11:39:58 -0400

    Comment out changes

--- src/main/webapp/scripts/admin.js
+++ src/main/webapp/scripts/admin.js
@@ -3,7 +3,6 @@ function submitShelter()
 	console.log("Sending request to add new shelter");
 	document.getElementById("shelterForm").submit();
 	alert("Sending request to add new shelter");
-	window.location.href = "/admin.html";
 }
 
 function submitDisaster()
@@ -11,6 +10,5 @@ function submitDisaster()
 	console.log("Sending request to add new disaster");
 	document.getElementById("disasterForm").submit();
 	alert("Sending request to add new disaster");
-	window.location.href = "/admin.html";
 }
 
